8bc2c6fc989c3edc583808b4b2685e1e9fe2b080
Raphael <raphaelkamaukamunyu@gmail.com>
2025-10-13 09:27:48 +0000

    Footer updates,removed the icons due to complexity

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,8 @@
+
 // src/components/Footer.tsx
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import { FaEnvelope, FaLeaf, FaChartLine } from 'react-icons/fa';
-import { IconType } from 'react-icons';
 import '../App.css';
-
-interface IconComponentProps {
-  icon?: IconType;
-  size?: number;
-}
-
-const IconComponent: React.FC<IconComponentProps> = ({ icon: Icon, size = 20 }) => {
-  if (!Icon) return null;
-  return <Icon size={size} />;
-};
 
 const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
@@ -28,15 +17,9 @@ const Footer: React.FC = () => {
             </p>
           </Col>
           <Col md={6} className='text-center text-md-right'>
-            <a href='/planting-log' className='footer-icon mx-2' title='Planting Log'>
-              <IconComponent icon={FaEnvelope} />
-            </a>
-            <a href='/impact-dashboard' className='footer-icon mx-2' title='Impact Dashboard'>
-              <IconComponent icon={FaChartLine} />
-            </a>
-            <a href='/map' className='footer-icon mx-2' title='View Map'>
-              <IconComponent icon={FaLeaf} />
-            </a>
+            <a href='/planting-log' className='footer-link mx-2'>Planting Log</a>
+            <a href='/impact-dashboard' className='footer-link mx-2'>Impact Dashboard</a>
+            <a href='/map' className='footer-link mx-2'>View Map</a>
           </Col>
         </Row>
       </Container>
@@ -45,4 +28,3 @@ const Footer: React.FC = () => {
 };
 
 export default Footer;
-// src/components/Footer.tsx
